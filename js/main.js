8a6f8ccace5aacb376f39846925abb2c262f32b8
Novemberman90 <alexledovit@gmail.com>
2025-03-07 08:31:55 +0200

    add caont in js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const MENU_BTN = document.querySelector('.menu__btn');
+/* .menu__btn--active */
+
+const MENU_LIST = document.querySelector('.menu__list');
+/* .menu__list--active */
+
+const POPUP = document.querySelector('.popup');
+/* popup--active */ 
